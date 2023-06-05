422841b899d5de345d100d82f3fa3759acf9c688
JordanCurnew <jordancurnew4@gmail.com>
2023-06-05 11:48:58 -0400

    initial commit

---
+++ precache-manifest.01f1f9aadcbb0bc94579b6f5ad9d7555.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe1d41c074a51ea2d103c49b6a7aac99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8827f92c1b74e47ec0c2",
+    "url": "/static/css/main.a014c08d.chunk.css"
+  },
+  {
+    "revision": "f45d3351645d26d6f663",
+    "url": "/static/js/2.4a2d4ba4.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.4a2d4ba4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8827f92c1b74e47ec0c2",
+    "url": "/static/js/main.7f745337.chunk.js"
+  },
+  {
+    "revision": "ea630fe81389fbceefbb",
+    "url": "/static/js/runtime-main.bf9f4589.js"
+  },
+  {
+    "revision": "4f8df9a29e0a95cfcdabed3e41841b92",
+    "url": "/static/media/BenHeadshot_circle.4f8df9a2.png"
+  },
+  {
+    "revision": "c675de3189390f04ed592f853f0922c9",
+    "url": "/static/media/FlyingKingLogo.c675de31.svg"
+  },
+  {
+    "revision": "261263defe3c1062e2e84b2f172b4106",
+    "url": "/static/media/MachineLearningSprintLogo.261263de.svg"
+  },
+  {
+    "revision": "879fcfde703a79fce9672edc2ce66ac1",
+    "url": "/static/media/ParallelFourierLogo.879fcfde.svg"
+  },
+  {
+    "revision": "c5f1f7e8950b10ae4c8f024483b83d72",
+    "url": "/static/media/WikiCrawlerLogo.c5f1f7e8.svg"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "7ebe479d7997744a6c76deabfa3e3e59",
+    "url": "/static/media/jordan-headshot-circle.7ebe479d.png"
+  },
+  {
+    "revision": "2a30aefb166a241459b82ac6a91bc902",
+    "url": "/static/media/logo basic.2a30aefb.svg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/static/media/logo.2810a88b.svg"
+  },
+  {
+    "revision": "cb653a4d28f5f9be5e08165e3dc26ddf",
+    "url": "/static/media/philip-headshot-circle.cb653a4d.png"
+  }
+]);
